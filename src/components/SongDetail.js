71f8e144eb790f226ba93c3e6982716cac5470c7
Max Stern <stern.maxwell@gmail.com>
2020-01-15 16:47:50 -0800

    song detail jsx

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = props => {
-  return <div></div>;
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div>Please select a song</div>;
+  }
+  return (
+    <div>
+      <h3>Details for:</h3>
+      <p>
+        Title: {song.title}
+        <br />
+        Duration: {song.duration}
+      </p>
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
   return {
-    selectedSong: state.selectedSong
+    song: state.selectedSong
   };
 };
 
